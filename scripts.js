322d7ae0dbafb01d21cd839e3055511c53dc3422
sergiogitt <sromero147@gmail.com>
2023-09-01 16:11:14 +0200

    Numeros con colores y arreglado fallo en la recursividad

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
-let tamanyo_tablero=5;
+let tamanyo_tablero=7;
 let array_tablero=[];
-const filas = 5;
-const columnas = 5;
+let minas=4;
 
 function generarTablero(minas,tamanyo){
     const tablero = document.getElementById('tablero');
-    for (let i = 0; i < filas; i++) {
+    for (let i = 0; i < tamanyo_tablero; i++) {
         let fila_nueva=[];
         const fila = document.createElement('tr');
-        for (let j = 0; j < columnas; j++) {
+        for (let j = 0; j < tamanyo_tablero; j++) {
         const columna = document.createElement('td');
         const boton = document.createElement('button');
         fila_nueva.push("")
@@ -21,50 +20,65 @@ function generarTablero(minas,tamanyo){
         boton.setAttribute('onclick', `abrirCasilla(${i},${j})`);
         }
         array_tablero.push(fila_nueva)
-        // Agrega un salto de línea después de cada fila (opcional)
         tablero.appendChild(fila);
         
     }
-    generarMinas(5);
+    generarMinas(3);
     generarNumeros();
+    
     console.log(array_tablero)
 }
 function abrirCasilla(fila,columna){
-    console.log("abirendo "+fila+ columna)
     let casilla=document.getElementById(`boton-${fila}-${columna}`);
     casilla.removeAttribute('onclick');
     casilla.classList.remove('activo');
     casilla.classList="inactivo";
     
-    let contenido=document.createTextNode(array_tablero[fila][columna]);
-    casilla.appendChild(contenido)
+    let contenido = document.createTextNode(array_tablero[fila][columna]);
+    if (array_tablero[fila][columna] === 0) {
+        contenido = document.createTextNode(" "); // Cambia el contenido a un espacio en blanco
+    }
+    switch(array_tablero[fila][columna]){
+        case 1:
+            console.log("hay un unao")
+            casilla.classList.add("blue");
+            break;
+        case 2:
+            casilla.classList.add("green");
+            break;
+        case 3:
+            casilla.classList.add("red");
+            break;
+    }
     console.log(contenido)
-    if(array_tablero[fila][columna]==0){
-        console.log("recorro vecinas")
-        recorrerVecinos(fila,columna)
+    casilla.appendChild(contenido);
 
+    if(array_tablero[fila][columna]==0){
+        recorrerVecinos(fila,columna)
     }
     
 }
-function recorrerVecinos(fila,columna){
-    
-    if(fila>=0&&fila<array_tablero.length&&columna>=0&&columna<array_tablero[0].length){
-        for ( i=fila-1;i<=fila+1;i++){
-            if (i>=0&&i<array_tablero.length){
+function recorrerVecinos(fila,columna){    
+   let i=0;
+   let j=0;
+        for (i=fila-1;i<=fila+1;i++){
+            if (i>=0&&i<tamanyo_tablero){
                 for ( j=columna-1;j<=columna+1;j++){
-                    if ((j>=0&&j<array_tablero[i].length)&&(!(fila==i&&columna==j))){
-                        console.log(array_tablero[i][j])
+                    if ((j>=0&&j<tamanyo_tablero)&&!(columna==j&&fila==i)){
+                        
                         let casilla=document.getElementById(`boton-${i}-${j}`);
-                        if (array_tablero[i][j]==0&&casilla.classList.contains("activo")){
+                        
+                        if (array_tablero[i][j]!="*"&&casilla.classList.contains("activo")){
                             abrirCasilla(i,j);
-                            console.log("abro mas vecinas")
                         }
+                        
 
                     }
                 }
             }
         }
-    }
+        
+    
 }
 function generarMinas(minas){
     numMinas=0;
@@ -78,7 +92,6 @@ function generarMinas(minas){
 
 
     }
-    console.log(array_tablero)
 }
 function generarNumeros(){
     for (let i = 0; i < tamanyo_tablero; i++) {
@@ -109,4 +122,4 @@ function contarMinasVecinas( fila,  columna) {
     }
     return numeroMinas;
 }
-window.onload=generarTablero(10,2);
+window.onload=generarTablero(5,5);
